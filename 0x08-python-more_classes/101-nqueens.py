debe4c1ac023dc9c41aa2bd7d982807fdb96eac0
Lizzy0295 <lizzyjacinta95@gmail.com>
2024-01-29 03:42:21 -0800

    corrested nqueens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,62 +1,74 @@
 #!/usr/bin/python3
-"""
-nqueens backtracking program to print the coordinates of n queens
-on an nxn grid such that they are all in non-attacking positions
-"""
+import sys
 
 
-from sys import argv
+class SolveNQueens:
+    def __init__(self, n):
+        # Attributes and variables
+        self.n = n
+        self.colSet = set()  # (c)  # Stores occupied columns
+        self.posDiagSet = set()  # (r+c)  # Stores occupied positive diagonals
+        self.negDiagSet = set()  # (r-c)  # Stores occupied negative diagonals
+        self.board = self.create_board()  # Chessboard representation
+        self.solution = []  # List to store valid solutions
+
+    def create_board(self):
+        # Creates an empty chessboard of size n x n
+        return [[" "] * self.n for i in range(self.n)]
+
+    def parse_solution(self, board):
+        # Parses the chessboard to extract the positions of queens
+        parsed_solution = []
+        for r in range(len(board)):
+            for c in range(len(board)):
+                if board[r][c] == "Q":
+                    parsed_solution.append([r, c])
+                    break
+        return parsed_solution
+
+    def solve_by_backtracking(self, r):
+        # Recursive function to solve the N-Queens problem using backtracking
+        if r == self.n:
+            parsed_solution = self.parse_solution(self.board)
+            self.solution.append(parsed_solution)
+            return
+        for c in range(self.n):
+            if c in self.colSet or (r + c) in self.posDiagSet:
+                continue
+            if (r - c) in self.negDiagSet:
+                continue
+
+            self.colSet.add(c)
+            self.posDiagSet.add(r + c)
+            self.negDiagSet.add(r - c)
+
+            self.board[r][c] = "Q"
+
+            self.solve_by_backtracking(r + 1)
+
+            self.colSet.remove(c)
+            self.posDiagSet.remove(r + c)
+            self.negDiagSet.remove(r - c)
+            self.board[r][c] = " "
+
+    def solve(self):
+        # Solves the N-Queens problem
+        self.solve_by_backtracking(0)
+        return self.solution
+
 
 if __name__ == "__main__":
-    a = []
-    if len(argv) != 2:
+    if len(sys.argv) != 2:
         print("Usage: nqueens N")
-        exit(1)
-    if argv[1].isdigit() is False:
+        sys.exit(1)
+    if not sys.argv[1].isdigit():
         print("N must be a number")
-        exit(1)
-    n = int(argv[1])
-    if n < 4:
+        sys.exit(1)
+    if int(sys.argv[1]) < 4:
         print("N must be at least 4")
-        exit(1)
+        sys.exit(1)
 
-    # initialize the answer list
-    for i in range(n):
-        a.append([i, None])
-
-    def already_exists(y):
-        """check that a queen does not already exist in that y value"""
-        for x in range(n):
-            if y == a[x][1]:
-                return True
-        return False
-
-    def reject(x, y):
-        """determines whether or not to reject the solution"""
-        if (already_exists(y)):
-            return False
-        i = 0
-        while(i < x):
-            if abs(a[i][1] - y) == abs(i - x):
-                return False
-            i += 1
-        return True
-
-    def clear_a(x):
-        """clears the answers from the point of failure on"""
-        for i in range(x, n):
-            a[i][1] = None
-
-    def nqueens(x):
-        """recursive backtracking function to find the solution"""
-        for y in range(n):
-            clear_a(x)
-            if reject(x, y):
-                a[x][1] = y
-                if (x == n - 1):  # accepts the solution
-                    print(a)
-                else:
-                    nqueens(x + 1)  # moves on to next x value to continue
-
-    # start the recursive process at x = 0
-    nqueens(0)
+    snq = SolveNQueens(int(sys.argv[1]))
+    solutions = snq.solve()
+    for solution in solutions:
+        print(solution)
